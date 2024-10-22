1ff1010d17753b4fc7f49377ae58647c2897eeaf
LeGhosdragon <simon.bou@bgp.ca>
2024-10-22 12:08:21 -0400

    TRANSFERT MARCHE PAS INACTIVITÉ == 300000000

--- src/main/java/com/atoudeft/banque/serveur/ServeurBanque.java
+++ src/main/java/com/atoudeft/banque/serveur/ServeurBanque.java
@@ -16,7 +16,7 @@ import java.util.ListIterator;
  * @since 2024-08-20
  */
 public class ServeurBanque extends Serveur {
-    public static final int DELAI_INACTIVITE = 30000;
+    public static final int DELAI_INACTIVITE = 3000000;
     //Référence vers la banque gérée par ce serveur :
     private Banque banque;
     //Thread qui supprime les connexions inactives :
